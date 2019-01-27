75c8c4c3223f4acf2b6cef0fcd443ad9ef6c8771
Sébastien Guillon <sebastienguillon@gmail.com>
2019-01-27 13:05:00 +0100

    Move ErrorHandlerService to services/

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,7 +6,7 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
 
-import { ErrorHandlerService } from './error-handler.service';
+import { ErrorHandlerService } from './services/error-handler.service';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
